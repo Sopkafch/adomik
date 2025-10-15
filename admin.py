fadea5a0cc2f11e9b12cc9c72f32e1a2bd0ecd63
SA0NT1 <86911179+Sopkafch@users.noreply.github.com>
2025-10-15 23:02:39 +0500

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,24 +1,4 @@
-# admin_socket.py
-import socket
-import threading
-import os
 
-def receive_messages(sock):
-    while True:
-        try:
-            data = sock.recv(1024)
-            if not data:
-                break
-            message = data.decode().strip()
-            print(message)
-
-        except:
-            break
-
-def check_file(file):
-    dir_list = os.listdir()
-    if file in dir_list:
-       return True
     else:
         return False
 
@@ -34,43 +14,7 @@ def uploadFile(sock):
         sock.send(f'{file_name} | {file_size}'.encode('utf-8'))
 
 
-        with open(file_name, 'rb') as f:
-            while True:
-                chunk = f.read(4096)
-                if not chunk:
-                    break
-                sock.sendall(chunk)
-
-        print('Файл отправлен')
-    else:
-        print('Файл не найден')
-
-
-
-def main():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect(("45.143.93.78", 10000))
-    sock.sendall(b'I8t12ok89u-k9u!@#e4')  # говорим серверу, что это админ
-
-    # Запускаем поток, который слушает сервер
-    threading.Thread(target=receive_messages, args=(sock,), daemon=True).start()
-
-    while True:
-        try: 
-            msg = input("$ ").strip()
-            if not msg:
-                continue
-
-            if msg == 'upload file':
-                uploadFile(sock)   
-                continue    
-
-            sock.sendall(msg.encode('utf-8'))
-
-        except (KeyboardInterrupt, EOFError):
-            print("\n[!] Выход")
-            break
-        except Exception as e:
+        with open
             print(f"[!] Ошибка: {e}")
             break
 
@@ -78,3 +22,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
